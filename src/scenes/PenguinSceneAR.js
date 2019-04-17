8192dea749087aaac57021825e189ef2e11293a5
Andrew <me@iamandrew.io>
2019-04-16 23:54:52 -0700

    add placeholder pinch and rotate functions for objects

--- src/scenes/PenguinSceneAR.js
+++ src/scenes/PenguinSceneAR.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
 import {
   ViroARScene,
-  ViroText,
   Viro3DObject,
   ViroAmbientLight,
   ViroSpotLight,
@@ -12,61 +10,84 @@ export default class PenguinSceneAR extends Component {
   constructor() {
     super();
     this.state = {
-      text: "Initializing AR..."
+      text: 'Initializing AR...',
+      pixelPenguin: {
+        scale: [.15, .15, .15],
+        rotation: [0, 0, 0]
+      },
+      linuxPenguin: {
+        scale: [.02, .02, .02],
+        rotation: [0, 0, 0]
+      }
     };
   }
 
+  handlePinch(pinchState, scaleFactor, source) {
+    console.log({pinchState})
+    console.log({scaleFactor})
+    console.log({source})
+    if(pinchState == 3) {
+    // update scale of obj by multiplying by scaleFactor  when pinch ends.
+      return;
+    }
+    //set scale using native props to reflect pinch.
+  }
+
+  handleRotate(rotateState, rotationFactor, source) {
+    console.log({rotateState})
+    console.log({rotationFactor})
+    console.log({source})
+    if (rotateState == 3) {
+      //set to current rotation - rotationFactor.
+      return;
+    }
+    //update rotation using setNativeProps
+  }
+
   render() {
+    const { linuxPenguin, pixelPenguin } = this.state;
     return (
       <ViroARScene onTrackingUpdated={() => this.setState({text: 'Penguins!'})}>
-        <ViroText
-          text={this.state.text}
-          scale={[.1, .1, .1]}
-          height={1}
-          width={4}
-          position={[0, .5, -1]}
-          style={styles.textStyle} />
 
-        <ViroAmbientLight color={"#eeeeee"} />
+        <ViroAmbientLight color={'#eeeeee'} />
+
         <ViroSpotLight
           innerAngle={5}
           outerAngle={90}
           direction={[0,-1,-.2]}
           position={[0, 3, 1]}
-          color="#ffffff"
+          color='#ffffff'
           castsShadow={true} />
 
         <Viro3DObject
           source={require('../assets/pixel-penguin/SM_Penguin_Emperor.obj')}
-          position={[0, 0, -1]}
-          scale={[.15, .15, .15]}
+          position={[0, 0, 1]}
+          scale={pixelPenguin.scale}
+          rotation={pixelPenguin.rotation}
           type='OBJ'
           dragType='FixedToWorld'
           onDrag={()=>{}}
           onError={console.log}
+          highAccuracyEvents
+          onPinch={this.handlePinch}
+          onRotate={this.handleRotate}
         />
 
         <Viro3DObject
           source={require('../assets/linux-penguin/Linux_Penguin.obj')}
           position={[0, 0, -1]}
-          scale={[.02, .02, .02]}
+          scale={linuxPenguin.scale}
+          rotation={linuxPenguin.rotation}
           type='OBJ'
           dragType='FixedToWorld'
           onDrag={()=>{}}
           onError={console.log}
+          highAccuracyEvents
+          onPinch={this.handlePinch}
+          onRotate={this.handleRotate}
         />
 
       </ViroARScene>
     );
   }
 };
-
-var styles = StyleSheet.create({
-  textStyle: {
-    fontFamily: 'Arial',
-    fontSize: 50,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-});
